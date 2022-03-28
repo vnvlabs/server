3b9d5f22c819a28006765f6b57f14732715d392f
ben <boneill@rnet-tech.com>
2022-03-28 14:30:14 -0400

    Updates

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-
-# First step is to build the serve docker image. This just install mongodb inside the base image and
-# copies over the launch script:
-
-docker build -t vnv_serve --build-arg VNV_BASE_IMAGE=$1 . 
-
-# Next step is to create a virtual python env and install the requirments
+# First step is to create a virtual python env and install the requirments
 virtualenv virt
-virt/bin/pip install -r src/requirements.txt
+virt/bin/pip install -r requirements.txt
+
+# Second step is to build the serve docker image. This just install mongodb inside the base image and
+# copies over the launch script:
+docker build -f docker/Dockerfile -t vnv_serve --build-arg VNV_BASE_IMAGE=$1 ./docker 
 
 #last step is to run the thing
 virt/bin/python src/run.py config.json
--- src/run.py
+++ src/run.py
@@ -3,22 +3,39 @@
 
 from app import create_serve_app
 import sys,json
+import os
+import shutil
 
 class Config:
     DEBUG=False
     port = 5001
     DOCKER_IMAGE = "vnv_serve"
     DEFAULT_USERS= {"Admin" : {"password" : "Admin", "admin" : True}}
-
+    
 
 app_config = Config()
 
 with open(sys.argv[1],'r') as c:
     co = json.load(c)
+    print(co)
     for k,v in co.items():
+
         try:
-            app_config[k] = v
-        except:
-            print("Could not configure option ", k, v)
+            setattr(app_config, k, v)
+        except Exception as e:
+            print(e, "Could not configure option ", k, v)
+
+
+try: 
+    shutil.copy(os.path.join(os.getcwd(),app_config.LOGO), "./src/app/static/assets/images/logo-dark.png")
+except Exception as e:
+    print(e)
+
+try: 
+    shutil.copy(os.path.join(os.getcwd(),app_config.ICON), "./src/app/static/assets/images/favicon.ico")
+except Exception as e:
+    print(e)
+
+
 socketio, app = create_serve_app(app_config)
 socketio.run(app, use_reloader=False, host="0.0.0.0", port=app_config.port)
