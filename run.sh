0e1df152c8648318cab6f70070390ef5a76f213a
ben <boneill@rnet-tech.com>
2022-05-31 15:42:21 -0400

    updates

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# First step is to create a virtual python env and install the requirments
-virtualenv --python=python3 virt
-virt/bin/pip install -r requirements.txt
-virt/bin/python src/run.py config.json
